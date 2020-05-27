2a2a7f9c9dd39f3a1546560f11a4a1b693a83099
Ahollow2 <40675596+Ahollow2@users.noreply.github.com>
2020-05-27 16:40:31 -0700

    Setting up
    Just figured out how to get an existing project into github!

---
+++ Impossible Game/Assets/Prefabs/GameManager.cs
@@ -0,0 +1,33 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    private bool gameHasEnded = false;
+    public float restartdelay = 1f;
+
+    public GameObject completeLevelUI;
+    
+    public void CompleteLevel()
+    {
+        completeLevelUI.SetActive(true);
+    }
+    public void Endgame()
+    {
+        if (gameHasEnded == false)
+        {
+
+            gameHasEnded = true;
+            Debug.Log("gameover");
+            Restart();
+            Invoke("Restart", restartdelay);
+        }
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ Impossible Game/Assets/Script/playerCollison.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerCollison : MonoBehaviour
+{
+    public playerMovement movement;
+
+    
+    
+    public void OnCollisionEnter(UnityEngine.Collision collisionInfo)
+    {
+        if (collisionInfo.gameObject.tag == "Enemy")
+        {
+            // Two ways to do this first one is commented
+            // movement.enabled = false;
+            GetComponent<playerMovement>().enabled = false;
+            FindObjectOfType<GameManager>().Endgame();
+
+        }
+        
+
+        
+        
+    }
+}
